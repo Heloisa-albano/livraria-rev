47804a04ad961ae42d5cca051db8ec8bd433ee86
Heloisa-albano <heloisalopesalbano@gmail.com>
2023-05-22 11:58:42 -0300

    add-img-legenda

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
 </script>
 <template>
   <h1>Minha livraria</h1>
-  <div class="livro">
-    {{ livros }}
+  <div class="livros">
+    <div class="card-livro" v-bind:on-beforeinput="livro in livros" :key="livro.id">
+      <img :src="livro.cover" class="livro-capa"/>
+      <p class="livro-titulo">{{livro.title}}</p>
+      <p class="livro-titulo">{{livro.author}}</p>
+      <p class="livro-titulo">{{livro.price}}</p>
+    </div>
   </div>
 </template>
 
