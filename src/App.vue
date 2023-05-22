d869deb445725009d97e92ba7fa861ef15a59478
Heloisa-albano <heloisalopesalbano@gmail.com>
2023-05-22 11:42:01 -0300

    aa

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import {livros} from '@/_data/livros'
+</script>
+<template>
+  <h1>Minha livraria</h1>
+  <div class="livro">
+    {{ livros }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/_data/livros.js
@@ -0,0 +1,38 @@
+export const livros = [
+    {
+      id: 1,
+      title: "O poderoso chefão",
+      author: "Mario Puzo",
+      price: 49.9,
+      cover: "https://m.media-amazon.com/images/I/71wy+cucv9L._AC_UL400_.jpg",
+    },
+    {
+      id: 2,
+      title: "O senhor dos Anéis",
+      author: "J R. R. Tolkien ",
+      price: 25.2,
+      cover: "https://m.media-amazon.com/images/I/81SM0D5+DwL._AC_UL400_.jpg",
+    },
+    {
+      id: 3,
+      title: "Assassins creed",
+      author: "Oliver Bowden e Ana Carolina Mesquita",
+      price: 31.1,
+      cover: "https://m.media-amazon.com/images/I/81nc-0VvdjL._AC_UL400_.jpg",
+    },
+    {
+      id: 4,
+      title: "Romance da onça dragona",
+      author: "Noam Chomsky",
+      price: 33.7,
+      cover: "https://m.media-amazon.com/images/I/61elszSZwHL._AC_UL400_.jpg",
+    },
+    {
+      id: 5,
+      title: "Bruxa Onilda é uma boa companheira",
+      author: "Roser Capdevila e Enric Larreula",
+      price: 43.2,
+      cover: "https://m.media-amazon.com/images/I/815QLZEU2uL._AC_UL400_.jpg",
+    },    
+  ]
+
